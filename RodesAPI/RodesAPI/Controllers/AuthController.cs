9d4a3ad6d204fa21126920921f8cc5e424dfc3cf
Raphael Maia <raphzmaia@gmail.com>
2017-07-03 11:19:41 -0300

    -Code trimming

--- RodesAPI/RodesAPI/Controllers/AuthController.cs
+++ RodesAPI/RodesAPI/Controllers/AuthController.cs
@@ -2,9 +2,7 @@
 using RodesAPI.Infra;
 using RodesAPI.Services;
 using RodesAPI.ViewModel;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;

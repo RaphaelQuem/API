68f65db96eb36935145a2c978d14e471617409ee
raphaelquem <raphzmaia@gmail.com>
2017-07-19 17:23:32 -0300

    ItemVM changed to have numbers properly formatted

--- RodesAPI/RodesAPI.ViewModel/ItemsVM.cs
+++ RodesAPI/RodesAPI.ViewModel/ItemsVM.cs
@@ -9,14 +9,14 @@
         public string Referencia { get; set; }
         public string DescReferencia { get; set; }
         public string DescricaoColecao { get; set; }
-        public decimal CMV { get; set; }
+        public string CMV { get; set; }
         public string DescricaoMoeda { get; set; }
         public string Situacao { get; set; }
-        public decimal PrecoVenda { get; set; }
-        public decimal PrecoEspecial { get; set; }
+        public string PrecoVenda { get; set; }
+        public string PrecoEspecial { get; set; }
         public decimal Saldo { get; set; }
         public string Peca { get; set; }
         public string Brinde { get; set; }
-        public decimal ValorPeca { get; set; }
+        public string ValorPeca { get; set; }
     }
 }

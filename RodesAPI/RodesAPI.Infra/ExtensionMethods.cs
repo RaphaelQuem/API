f960cbbb7cdf370115c5ad06d2141af5bc96ad7f
raphaelquem <raphzmaia@gmail.com>
2017-07-20 13:36:13 -0300

    ItemVM changed to have numbers properly formatted

--- RodesAPI/RodesAPI.Infra/ExtensionMethods.cs
+++ RodesAPI/RodesAPI.Infra/ExtensionMethods.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Net;
 using System.Net.Http.Headers;
 using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace RodesAPI.Infra
 {
